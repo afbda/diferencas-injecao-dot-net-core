27fada1f8cab239595b51167ba62cacd86aa0084
Aloisio Furbino Bretas de Almeida <40540686+afbda@users.noreply.github.com>
2021-10-08 00:01:09 -0300

    Adição testes unitários função get

--- DependencyInjectionExamplesTest/UnitTest1.cs
+++ DependencyInjectionExamplesTest/UnitTest1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using DependencyInjectionExamples;
+using DependencyInjectionExamples.Controllers;
+using DependencyInjectionExamples.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace DependencyInjectionExamplesTest
 {
     public class UnitTest1
     {
+        private readonly Mock<ILogger<WeatherForecastController>> loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        private readonly Mock<IServiceProvider> serviceProviderMock = new Mock<IServiceProvider>();
+        private readonly Mock<IWeatherForecastService> weatherForecastServiceMock = new Mock<IWeatherForecastService>();
         [Fact]
-        public void Test1()
+        public void ShouldReturnAWeatherForecastArray()
         {
-
+            var controller = new WeatherForecastController(loggerMock.Object, serviceProviderMock.Object);
+            weatherForecastServiceMock.Setup(x => x.Get()).Returns(new List<WeatherForecast>() { new WeatherForecast() { Date = DateTime.Now, Summary = "Test", TemperatureC = 10 } });
+            Assert.NotEmpty(controller.Get(weatherForecastServiceMock.Object));
         }
     }
 }
