16281f078d52d41750e3aa237aa8f96a6c6ae6e0
Aloisio Furbino Bretas de Almeida <40540686+afbda@users.noreply.github.com>
2021-10-07 23:44:02 -0300

    Adição projeto de testes

---
+++ DependencyInjectionExamplesTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace DependencyInjectionExamplesTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
